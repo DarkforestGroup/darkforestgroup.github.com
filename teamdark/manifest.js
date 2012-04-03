d509ec15a7ed73f381cd6da8cf8ae82cc23c80c4
Keiran Collins <keiran@darkforestgroup.com>
2012-04-03 09:58:42 +0100

    added releases for pyramid and zeus

--- teamdark/manifest.js
+++ teamdark/manifest.js
@@ -1,17 +1,29 @@
 {
 "zeus": [
     {
-        "romid": "Darkforest ROM v4",
-        "description": "Version 4.1.11",
-        "link": "http://somewhere.com",
-	"file": "somewhere.zip"
+        "romid": "Darkforest ROM v4 ICS",
+        "description": "Version 4.1.11, Xperia Play GSM, Android 4.0.3, AOSP-like",
+        "link": "http://icxperia.com/keiran/DFRv4/DFRv4.1.11.zip",
+	"file": "DFRv4.1.11.zip"
     }
  ],
 "pyramid": [
     {
-        "romid": "ROMiD",
-        "description": "Description",
-        "link": "http://somewhere.com",
-	"file": "somewhere.zip"
+        "romid": "DarkForest Sensation XE 6.1.0",
+        "description": "Version 6.1.0 ICS, Original Sensation, XE and 4G, Android 4.0.3, Sense 3.6",
+        "link": "http://icxperia.com/jonny/DarkForest%20Sensation/v6.1.0/DarkForest%20Sensation%20XE%20ICS%206.1.0.zip",
+	"file": "DarkForestSensationXEv6.1.0.zip"
+    },
+    {
+        "romid": "DarkForest Sensation XE 5.5.0",
+        "description": "Version 5.5.0 ICS, Original Sensation, XE and 4G, Android 4.0.3, Sense 3.6",
+        "link": "http://icxperia.com/jonny/DarkForest%20Sensation/v5.5.0/DarkForest%20Sensation%20XE%20ICS%205.5.0.zip",
+	"file": "DarkForestSensationXEv5.5.0.zip"
+    },
+    {
+        "romid": "DarkForest Sensation XE 5.3.0",
+        "description": "Version 5.3.0 ICS, Original Sensation, XE and 4G, Android 4.0.3, Sense 3.6",
+        "link": "http://icxperia.com/jonny/DarkForest%20Sensation/v5.3.0/DarkForest%20Sensation%20XE%20ICS%205.3.0.zip",
+	"file": "DarkForestSensationXEv5.3.0.zip"
     }
  ]}
