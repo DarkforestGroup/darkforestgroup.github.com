0ed0e04bcf0f165076ba5c5cacb7e42f1a4ccb7d
Keiran Collins <keiran@darkforestgroup.com>
2012-04-08 14:44:50 +0100

    update

--- teamdark/manifest.js
+++ teamdark/manifest.js
@@ -1,35 +1,47 @@
 {
 "zeus": [
     {
-        "romid": "Darkforest ROM v4.1.23 ICS",
-        "description": "Version 4.1.23, Xperia Play GSM, Android 4.0.3, AOSP-like, CM Music and Trebuchet",
-        "link": "http://icxperia.com/keiran/DFRv4/DFRv4.1.23.zip",
+	"romid": "Darkforest ROM v4.1.23 ICS",
+	"description": "Version 4.1.23, Xperia Play GSM, Android 4.0.3, AOSP-like, CM Music and Trebuchet",
+	"link": "http://icxperia.com/keiran/DFRv4/DFRv4.1.23.zip",
 	"file": "DFRv4.1.23.zip"
     },    
     {
-        "romid": "Darkforest ROM v4.1.11 ICS",
-        "description": "Version 4.1.11, Xperia Play GSM, Android 4.0.3, AOSP-like",
-        "link": "http://icxperia.com/keiran/DFRv4/DFRv4.1.11.zip",
+	"romid": "Darkforest ROM v4.1.11 ICS",
+	"description": "Version 4.1.11, Xperia Play GSM, Android 4.0.3, AOSP-like",
+	"link": "http://icxperia.com/keiran/DFRv4/DFRv4.1.11.zip",
 	"file": "DFRv4.1.11.zip"
+    },
+    {
+	"romid": "Google Apps ICS Signed",
+	"description": "Google Apps - 17/03/12 - Face Unlock - For Android 4.0+",
+	"link": "http://icxperia.com/keiran/gapps/gapps-ics-20120317-signed.zip",
+	"file": "gapps-ics-20120317-signed.zip"
     }
  ],
 "pyramid": [
     {
-        "romid": "DarkForest Sensation XE 6.1.0",
-        "description": "Version 6.1.0 ICS, Original Sensation, XE and 4G, Android 4.0.3, Sense 3.6",
-        "link": "http://icxperia.com/jonny/DarkForest%20Sensation/v6.1.0/DarkForest%20Sensation%20XE%20ICS%206.1.0.zip",
+	"romid": "DarkForest Sensation XE 6.1.0",
+	"description": "Version 6.1.0 ICS, Original Sensation, XE and 4G, Android 4.0.3, Sense 3.6",
+	"link": "http://icxperia.com/jonny/DarkForest%20Sensation/v6.1.0/DarkForest%20Sensation%20XE%20ICS%206.1.0.zip",
 	"file": "DarkForestSensationXEv6.1.0.zip"
     },
     {
-        "romid": "DarkForest Sensation XE 5.5.0",
-        "description": "Version 5.5.0 ICS, Original Sensation, XE and 4G, Android 4.0.3, Sense 3.6",
-        "link": "http://icxperia.com/jonny/DarkForest%20Sensation/v5.5.0/DarkForest%20Sensation%20XE%20ICS%205.5.0.zip",
+	"romid": "DarkForest Sensation XE 5.5.0",
+	"description": "Version 5.5.0 ICS, Original Sensation, XE and 4G, Android 4.0.3, Sense 3.6",
+	"link": "http://icxperia.com/jonny/DarkForest%20Sensation/v5.5.0/DarkForest%20Sensation%20XE%20ICS%205.5.0.zip",
 	"file": "DarkForestSensationXEv5.5.0.zip"
     },
     {
-        "romid": "DarkForest Sensation XE 5.3.0",
-        "description": "Version 5.3.0 ICS, Original Sensation, XE and 4G, Android 4.0.3, Sense 3.6",
-        "link": "http://icxperia.com/jonny/DarkForest%20Sensation/v5.3.0/DarkForest%20Sensation%20XE%20ICS%205.3.0.zip",
+	"romid": "DarkForest Sensation XE 5.3.0",
+	"description": "Version 5.3.0 ICS, Original Sensation, XE and 4G, Android 4.0.3, Sense 3.6",
+	"link": "http://icxperia.com/jonny/DarkForest%20Sensation/v5.3.0/DarkForest%20Sensation%20XE%20ICS%205.3.0.zip",
 	"file": "DarkForestSensationXEv5.3.0.zip"
+    },
+    {
+	"romid": "Google Apps ICS Signed",
+	"description": "Google Apps - 17/03/12 - Face Unlock - For Android 4.0+",
+	"link": "http://icxperia.com/keiran/gapps/gapps-ics-20120317-signed.zip",
+	"file": "gapps-ics-20120317-signed.zip"
     }
  ]}
